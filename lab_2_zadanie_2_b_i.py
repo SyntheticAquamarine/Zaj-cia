cf597011627f884b042f42605d167964e1503c60
ArtificialAquamarine <janjasielski.j@gmail.com>
2021-10-31 21:49:37 +0100

    Zadania laboratoria 2

---
+++ lab_2_zadanie_2_b_i.py
@@ -0,0 +1,17 @@
+#Podpunkt I Ver 1
+numbers = [1,2,3,4,5]
+def number(numbers):
+    for number in numbers:
+        print(number*2)
+number(numbers)
+
+#Podpunkt I Ver 2
+
+lista = []
+for i in range(5):
+    lista.append(int(input("Proszę podaj liczbę ")))
+
+def liczby(lista):
+    for liczba in lista:
+        print(liczba*2)
+liczby(lista)
---
+++ lab_2_zadanie_2_c.py
@@ -0,0 +1,5 @@
+numbers = [8,4,54,77,92,44,91,19,72,43]
+
+for count, i in enumerate(numbers):
+    if i%2==0:
+        print(f'iteracja nr.{count}, Wartość {i}')
